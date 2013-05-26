1fd59aece9a8dad3fbd18860cbc7d9932ee40834
sviridenkovalera <cowr.valera@gmail.com>
2013-05-26 14:57:42 +0400

    Add comments and restruc code

--- brainfuck.py
+++ brainfuck.py
@@ -1,44 +1,45 @@
 import sys
-
-str = ''
-for line in sys.stdin:
-    str += line.replace ( ' ', '')
-cmdIndex = 0
-cmd = ''
-index = 0
-memMap = []
-memMap.append (0)
-ceillSize = 256
-loopsMap = []
-
-
-while cmdIndex < len ( str ):
-    cmd = str [cmdIndex]
-    memMap [index] %= ceillSize
+#l-value of program
+cmd            = ''  #current command
+code           = ''  #input program code
+point          = 0   #point ceill of memory map
+cmdIndex       = 0   #index of current command
+ceillSize      = 256 #size single ceill
+loopsMap       = []  #store cmdIndex + 1 - begin loop 
+memMap         = [0] #memory map
+commandMap     = ('+', '-', '<', '>', '[', ']', '.', ',') # all commad BF language 
+#input program code
+for byte in sys.stdin.read(1):
+    if byte in commandMap:
+        code += byte
+#processing code 
+while cmdIndex < len ( code ):
+    cmd = code [cmdIndex]
+    memMap [point] %= ceillSize
     if cmd == '<':
-        index -= 1
-        if index < 0:
-            print ( 'Error index < 0')
+        point -= 1
+        if point < 0:
+            print ( 'Error index < 0' )
             sys.exit ( - 1 )
     elif cmd == '>':
         index += 1
         if index == len ( memMap ):
             memMap.append ( 0 )
     elif cmd == '+':
-        memMap [index] += 1
+        memMap [point] += 1
     elif cmd == '-':
-        memMap [index] -= 1
+        memMap [point] -= 1
     elif cmd == '.':
-        sys.stdout.write ( chr ( memMap [index] ) )
+        sys.stdout.write ( chr ( memMap [point] ) )
     elif cmd == ',':
-        memMap [index] = ord ( sys.stdin.read (1) )
+        memMap [point] = ord ( sys.stdin.read (1) )
     elif cmd == '[':
         loopsMap.append ( cmdIndex + 1 )
     elif cmd == ']':
-        if memMap [index] == 0:
+        if memMap [point] == 0:
             loopsMap.pop ()
         else:
-            cmdIndex = loopsMap [ len ( loopsMap ) - 1]
+            cmdIndex = loopsMap [len ( loopsMap ) - 1]
             continue
     cmdIndex += 1
 print ('')       
