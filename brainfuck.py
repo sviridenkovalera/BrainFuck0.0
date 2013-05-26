9fc34738a72bcfceb596cdceffb128c45c2cd50c
Valerii <cowr.valera@gmail.com>
2013-05-26 22:40:29 +0400

    Fix bug in LOOPS and ADDED two function + DEBUG MODE

--- brainfuck.py
+++ brainfuck.py
@@ -1,4 +1,26 @@
+_DEBUG = False
+_ADD_FUNCTION = False
+_SHOW_FORMT_CODE = False
+_SHOW_CLINE_CODE = True
 import sys
+def clineCode ( code ):
+    print ( 'Cliner code:')
+    print ( code )
+    print ('')
+def formatCode ( code ):
+    shift = 0
+    add_shift = 4
+    for i in code:
+        if i == '[':
+            shift += add_shift
+        for j in range ( shift ):
+            sys.stdout.write ( '    ')
+        if i == ']':
+            shift -= add_shift
+        if i not in ( '[', ']'):
+            sys.stdout.write ( '    ')
+        print ( str ( i ) )
+    print ('')
 #l-value of program
 cmd            = ''  #current command
 code           = ''  #input program code
@@ -13,43 +35,37 @@ commandMap     = ('+', '-', '<', '>', '[', ']', '.', ',') # all commad BF langua
 for byte in sys.stdin.read():
     if byte in commandMap:
         code += byte
-"""shift = 0
-add_shift = 4
-for i in code:
-    if i == '[':
-        shift += add_shift
-    for j in range ( shift ):
-        sys.stdout.write ( '    ')
-    if i == ']':
-        shift -= add_shift
-    if i not in ( '[', ']'):
-        sys.stdout.write ( ' ')
-    print ( str ( i ) )
-sys.exit ( 0 )"""
-
+if _ADD_FUNCTION:
+    if _SHOW_FORMT_CODE:
+        formatCode ( code )
+    if _SHOW_CLINE_CODE:
+        clineCode ( code )
+    sys.exit (0)
 steps_all = 0
 #processing code 
 while cmdIndex < len ( code ):
-    print ( 'Step: ' + str ( steps_all) )
-    print ( 'Current point: ' + str ( cmdIndex ) )
-    print ( 'Command: ' + code [ cmdIndex ] )
-    steps_all += 1
-    print ('****************************')
-    _cnt = 10
-    _min = point - _cnt
-    if _min < 0:
-        _min = 0
-    _max = point + _cnt
-    if _max >= maxSizeMemMap:
-        _max = maxSizeMemMap - 1
-    for prtIndex in range ( _min, _max + 1 ):
-        if prtIndex != point:
-            print ( "#" + str ( prtIndex) + ' Value: ' + str ( memMap [prtIndex]) )
-        else:
-            print ( "[ #" + str ( prtIndex) + ' Value: ' + str ( memMap [prtIndex]) + str (' ]') )
-    print ('*****************************')
-    print ('')
-    sys.stdin.read (1)
+    if _DEBUG:
+        print ( 'Step: ' + str ( steps_all) )
+        print ( 'Current point: ' + str ( cmdIndex ) )
+        print ( 'Command: ' + code [ cmdIndex ] )
+        steps_all += 1
+        print ('****************************')
+        _cnt = 10
+        _min = point - _cnt
+        if _min < 0:
+            _min = 0
+        _max = point + _cnt
+        if _max >= maxSizeMemMap:
+            _max = maxSizeMemMap - 1
+        for prtIndex in range ( _min, _max + 1 ):
+            if prtIndex != point:
+                print ( "#" + str ( prtIndex) + ' Value: ' + str ( memMap [prtIndex]) )
+            else:
+                print ( "[ #" + str ( prtIndex) + ' Value: ' + str ( memMap [prtIndex]) + str (' ]') )
+        print ('*****************************')
+        print ('')
+        if code [cmdIndex] != ',':
+            sys.stdin.read (1)
     cmd = code [cmdIndex]
     if cmd == '<':
         point -= 1
@@ -67,20 +83,33 @@ while cmdIndex < len ( code ):
         if memMap [point] < 0:
             memMap [point] = ceillSize - 1
     elif cmd == '.':
-        print ('Print value:')
+        if _DEBUG:
+            print ('Print value:    ')
         sys.stdout.write ( chr ( memMap [point] ) )
     elif cmd == ',':
-        print ('Input value:')
+        if _DEBUG:
+            print ('Input value:')
         memMap [point] = ord ( sys.stdin.read (1) )
         memMap [point] %= ceillSize
     elif cmd == '[':
-        loopsMap.append ( cmdIndex )
-    elif cmd == ']':
-        if memMap and memMap [point] == 0:
-            loopsMap.pop ()
-        else:
-            cmdIndex = loopsMap [len ( loopsMap ) - 1] + 1
+        if memMap [point] == 0:
+            cmdIndex += 1
+            _cnt = 1
+            while cmdIndex < len ( code ) and _cnt > 0:
+                if code [cmdIndex] == '[':
+                    _cnt += 1
+                elif code [cmdIndex] == ']':
+                    _cnt -= 1
+                cmdIndex += 1
             continue
+        else:
+            loopsMap.append ( cmdIndex )
+    elif cmd == ']':
+            if memMap [point] == 0:
+                loopsMap.pop ()
+            else:
+                cmdIndex = loopsMap [len ( loopsMap ) - 1] + 1
+                continue
     cmdIndex += 1
 print ('')       
      
